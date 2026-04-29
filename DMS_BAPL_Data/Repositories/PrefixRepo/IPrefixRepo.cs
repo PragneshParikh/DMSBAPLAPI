0511585de03b05d8b065736fd5dcf4c28703da13
Ajay Patel <it5.vad@rrglobal.com>
2026-04-29 15:38:50 +0530

    fix: prefix master implements new methods for get and get next number

--- DMS_BAPL_Data/Repositories/PrefixRepo/IPrefixRepo.cs
+++ DMS_BAPL_Data/Repositories/PrefixRepo/IPrefixRepo.cs
@@ -6,9 +6,12 @@ namespace DMS_BAPL_Data.Repositories.PrefixRepo
 {
     public interface IPrefixRepo
     {
+        Task<IEnumerable<NumberSequence>> Get();
         Task<PagedResponse<NumberSequence>> GetPrefixByPagedAsync(string? searchTerm, int pageIndex, int pageSize);
         Task<IEnumerable<NumberSequence>> GetPrefixByDealerCode(string dealerCode);
+        Task<NumberSequence> GetPrefixByDealerCodeModuleName(string dealerCode, string moduleName);
         Task<int> InsertPrefix(NumberSequenceViewModel numberSequence);
         Task<int> AddPrefixForDealers(NumberSequenceViewModel numberSequenceViewModel);
+        Task<int> UpdateNextNumberByDealerByModule(string dealerCode, string moduleName);
     }
 }
