810603c7d31af03a984a1c622d55da44b2561761
Ajay Patel <it5.vad@rrglobal.com>
2026-05-18 17:57:10 +0530

    fix: update vehicle inward API Method accept single item instead of multiple items

--- DMS_BAPL_Data/Repositories/VehicleInwardRepo/IVehicleInwardRepo.cs
+++ DMS_BAPL_Data/Repositories/VehicleInwardRepo/IVehicleInwardRepo.cs
@@ -1,4 +1,5 @@
 ﻿using DMS_BAPL_Data.DBModels;
+using DMS_BAPL_Utils.ViewModels;
 using Org.BouncyCastle.Bcpg.OpenPgp;
 using System;
 using System.Collections.Generic;
@@ -8,12 +9,12 @@ using System.Threading.Tasks;
 
 namespace DMS_BAPL_Data.Repositories.VehicleDispatchRepo
 {
-    public interface IVehicleDispatchRepo
+    public interface IVehicleInwardRepo
     {
         Task<IEnumerable<VehicleInward>> Get();
         Task<IEnumerable<VehicleInward>> GetVehicleByStatus(string dealerCode, Boolean status);
         Task<bool> UpdateInvoiceStatus(string invoiceNo, string userId);
-        Task<bool> InsertVehicleDispatchDetail(List<VehicleInward> vehicleDispatches);
+        Task<bool> InsertVehicleDispatchDetail(VehicleInwardViewModel vehicleInwardViewModel);
         Task<List<VehicleInward>> GetByChassisNosAsync(List<string> chassisNos);
     }
 }
---
+++ DMS_BAPL_Utils/Helpers/DateOnlyJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DMS_BAPL_Utils.Helpers
+{
+    public class DateOnlyJsonConverter : JsonConverter<DateOnly?>
+    {
+        private const string Format = "dd/MM/yyyy";
+
+        public override DateOnly? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+
+            return DateOnly.ParseExact(value, Format, CultureInfo.InvariantCulture);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateOnly? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+                writer.WriteStringValue(value.Value.ToString(Format));
+            else
+                writer.WriteNullValue();
+        }
+    }
+}
