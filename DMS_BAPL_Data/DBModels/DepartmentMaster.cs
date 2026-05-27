2b9847b0be1a7f44f875629884a281fc119840d2
Ajay Patel <it5.vad@rrglobal.com>
2026-05-27 14:43:36 +0530

    fix: scaffold the dbmodels

---
+++ DMS_BAPL_Data/DBModels/DepartmentMaster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DMS_BAPL_Data.DBModels;
+
+public partial class DepartmentMaster
+{
+    public int DepartmentId { get; set; }
+
+    public string DepartmentCode { get; set; } = null!;
+
+    public string DepartmentName { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public string? ModifiedBy { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+
+    public virtual ICollection<DesignationMaster> DesignationMasters { get; set; } = new List<DesignationMaster>();
+}
