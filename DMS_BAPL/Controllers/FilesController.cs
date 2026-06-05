46bb3c526d9f46785f2cee8f22ec40e7cecbb6d9
Ajay Patel <it5.vad@rrglobal.com>
2026-06-05 11:48:53 +0530

    fix: added new controller to upload videos, images, docs

---
+++ DMS_BAPL/Controllers/FilesController.cs
@@ -0,0 +1,39 @@
+﻿using DMS_BAPL_Data.Services.FileService;
+using DMS_BAPL_Utils.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMS_BAPL_Api.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly IFileService _fileService;
+
+        public FilesController(IFileService filesService)
+        {
+            _fileService = filesService;
+        }
+
+        [HttpGet("generate-upload-url")]
+        public async Task<IActionResult> GenerateUploadUrl(string fileName)
+        {
+            try
+            {
+                string userId = GetUserInfoFromToken.GetUserIdFromToken(HttpContext);
+
+                if (string.IsNullOrEmpty(userId))
+                    return Unauthorized("User not authorized");
+
+                var result = await _fileService.GenerateUploadUrl(fileName);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
