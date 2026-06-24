9f245e575cbbd5aa54590b860f18482a2808c1f6
MMbg2026 <mayank.maheshwari@bgauss.com>
2026-06-24 15:55:03 +0530

    Print -Layout-Changes

---
+++ DMS_BAPL_Utils/ViewModels/RoleWithCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS_BAPL_Utils.ViewModels
+{
+    public class RoleWithCategoryViewModel
+    {
+        public string Name { get; set; } = string.Empty;      // role name
+        public string Category { get; set; } = string.Empty;  // 'Sales' / 'Service'
+    }
+}
